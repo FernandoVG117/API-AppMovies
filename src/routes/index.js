25d10481c2cb0a07bf46a81a89981b5a71d961e4
Fernando <ing.fernandovg@gmail.com>
2024-08-03 22:03:20 -0600

    v1.1.0 To create Artist model along with its controller and router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const routerGenre = require('./genre.router');
+const routerArtist = require('./artist.router');
 const router = express.Router();
 
 router.use('/genres', routerGenre);
+router.use('/artists', routerArtist)
 
 module.exports = router;
