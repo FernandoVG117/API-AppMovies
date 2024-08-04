0409162ebbd1c1a5265bbc92d045fa4d8ccd68e9
Fernando <ing.fernandovg@gmail.com>
2024-08-03 22:24:54 -0600

    v1.2.0 To create Director model alongs with its controller and router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const routerGenre = require('./genre.router');
 const routerArtist = require('./artist.router');
+const routerDirector = require('./director.router');
 const router = express.Router();
 
 router.use('/genres', routerGenre);
-router.use('/artists', routerArtist)
+router.use('/artists', routerArtist);
+router.use('/directors', routerDirector);
 
 module.exports = router;
