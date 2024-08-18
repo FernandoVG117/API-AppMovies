df1688997099840af769fb2f12e99ccd0537811b
Fernando <ing.fernandovg@gmail.com>
2024-08-17 22:49:16 -0600

    testing 18 GetOne Movie

--- src/tests/movie.test.js
+++ src/tests/movie.test.js
@@ -20,6 +20,8 @@ test("POST --> BASE_URL, should return statusCode 201, and res.body.name === mov
         .post(`${BASE_URL}`)
         .send(movie)
 
+        movieId = res.body.id
+
         // console.log(res.body)
 
         expect(res.status).toBe(201)
@@ -35,7 +37,7 @@ test("GET --> BASE_URL, should return statusCode 200, and res.body.length === 1"
     const res = await request(app)
         .get(`${BASE_URL}`)
 
-        console.log(res.body)
+        // console.log(res.body)
 
         expect(res.status).toBe(200)
         expect(res.body).toBeDefined()
@@ -47,4 +49,23 @@ test("GET --> BASE_URL, should return statusCode 200, and res.body.length === 1"
         expect(res.body[0].genres).toHaveLength(0)
         expect(res.body[0].directors).toHaveLength(0)
         expect(res.body[0].actors).toHaveLength(0)
+})
+
+    // GET --> (GetOne)
+test("GET --> BASE_URL/:id, should return statusCode 200, and res.body.name === movie.name", async() => {
+    const res = await request(app)
+        .get(`${BASE_URL}/${movieId}`)
+
+        console.log(res.body)
+
+        expect(res.status).toBe(200)
+        expect(res.body).toBeDefined()
+        expect(res.body.name).toBe(movie.name)
+        expect(res.body.image).toBe(movie.image)
+        expect(res.body.synopsis).toBe(movie.synopsis)
+        expect(res.body.releaseYear).toBe(movie.releaseYear)
+
+        expect(res.body.genres).toHaveLength(0)
+        expect(res.body.directors).toHaveLength(0)
+        expect(res.body.actors).toHaveLength(0)
 })
