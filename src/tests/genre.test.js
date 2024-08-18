702ae82b4a0f4af89b924658f61bb67f1b4aa703
Fernando <ing.fernandovg@gmail.com>
2024-08-17 21:02:01 -0600

    testing 12 GetAll Genre

--- src/tests/genre.test.js
+++ src/tests/genre.test.js
@@ -17,7 +17,22 @@ test("POST -> BASE_URL, should return statusCode 201, and res.body.name === genr
         .post(`${BASE_URL}`)
         .send(genre)
 
+        genreId = res.body.id
+
         // console.log(res.body)
 
         expect(res.status).toBe(201)
+})
+
+    // GET --> (GetAll)
+test("GET -> BASE_URL, shoul return statusCode 200, and res.body.length === 1", async() => {
+    const res = await request(app)
+        .get(`${BASE_URL}`)
+
+        // console.log(res.body)
+
+        expect(res.status).toBe(200)
+        expect(res.body).toBeDefined()
+        expect(res.body).toHaveLength(1)
+        expect(res.body[0].name).toBe(genre.name)
 })
